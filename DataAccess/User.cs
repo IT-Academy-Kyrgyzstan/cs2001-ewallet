4b699106db8727cc331206101316a062cc513eba
Doron Doron <doronkg@yandex.ru>
2021-01-26 02:05:50 +0600

    create User model

---
+++ DataAccess/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataAccess
+{
+    public class User
+    {
+        [Key]        
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        [StringLength(20, MinimumLength = 6)]        
+        public string Password { get; set; }
+        public string Login { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
