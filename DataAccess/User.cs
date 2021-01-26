2ed7a53c3351543e9538e153a7fe7ac7c4d2751d
Doron Doron <doronkg@yandex.ru>
2021-01-26 17:32:20 +0600

    fix data annotation UserModel

--- DataAccess/User.cs
+++ DataAccess/User.cs
@@ -8,12 +8,20 @@ namespace DataAccess
     {
         [Key]        
         public int Id { get; set; }
+
+        [MaxLength(50)]
         public string FirstName { get; set; }
+
+        [MaxLength(50)]
         public string LastName { get; set; }
 
-        [StringLength(20, MinimumLength = 6)]        
+        [StringLength(50, MinimumLength = 6)]        
         public string Password { get; set; }
+
+        [StringLength(20, MinimumLength = 3)]
         public string Login { get; set; }
-        public DateTime Created { get; set; }
+        
+        [Column(TypeName = "DateTime")]
+        public string Created { get; set; }
     }
 }
