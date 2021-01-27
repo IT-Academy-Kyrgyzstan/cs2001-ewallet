a86ddb28638b67bc32c0d38d661926e5cdf9c532
Doron Doron <doronkg@yandex.ru>
2021-01-27 19:53:24 +0600

    fix delete attribut display

--- DataAccess/User.cs
+++ DataAccess/User.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace DataAccess
 {
@@ -9,23 +7,18 @@ namespace DataAccess
         [Key]        
         public int Id { get; set; }
 
-        [Display(Name = "Имя")]
         [MaxLength(50)]
         public string FirstName { get; set; }
 
-        [Display(Name = "Фамилия")]
         [MaxLength(50)]
         public string LastName { get; set; }
 
-        [Display(Name = "Пароль")]
         [StringLength(50, MinimumLength = 6)]        
         public string Password { get; set; }
 
-        [Display(Name = "Логин")]
         [StringLength(20, MinimumLength = 3)]
         public string Login { get; set; }
-
-        [Display(Name = "Дата создания")]
+               
         public string Created { get; set; }
     }
 }
