ea8bc9a0e19fc41d796f20fb5e4662ce06652c8d
Doron Doron <doronkg@yandex.ru>
2021-01-28 00:40:49 +0600

    delete sql script

---
+++ DataAccess/User.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DataAccess
+{
+    public class User
+    {
+        [Key]        
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        [StringLength(50, MinimumLength = 6)]        
+        public string Password { get; set; }
+
+        [StringLength(20, MinimumLength = 3)]
+        public string Login { get; set; }
+               
+        public string Created { get; set; }
+    }
+}
