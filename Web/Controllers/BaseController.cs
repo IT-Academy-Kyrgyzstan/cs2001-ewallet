b648fab5d77d5d20a147f61bf642cebeced7c8d8
Tilek Kasymov <tilek.kasymov.01@gmail.com>
2021-02-21 23:02:12 +0600

    transfer money

---
+++ Web/Controllers/BaseController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using DataAccess;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Web.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected readonly ILogger<HomeController> _logger;
+        protected readonly EwalletContext db;
+        public BaseController(ILogger<HomeController> logger, EwalletContext db)
+        {
+            _logger = logger;
+            this.db = db;
+        }
+        protected int GetUserId()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var result = int.Parse(userId);
+
+            return result;
+        }
+    }
+}
---
+++ Web/Controllers/TransferController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataAccess;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    [Authorize]
+    public class TransferController : BaseController
+    {
+        public TransferController(ILogger<HomeController> logger, EwalletContext db) : base(logger, db)
+        {
+        }
+
+        public IActionResult Transfer()
+        {
+            var userCard = db.CardAccounts.Where(u => u.UserId == GetUserId());
+            ViewBag.Card = userCard;
+            
+            return View();
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> Transfer(CardAccountModel cardAccountmodel)
+        {
+            var userCard = await db.CardAccounts.FirstOrDefaultAsync(u => u.CardNumber == cardAccountmodel.SelectCardNumber);
+            var transferCard = await db.CardAccounts.FirstOrDefaultAsync(u => u.CardNumber == cardAccountmodel.TransferCardNumber);
+
+            var userCardForViewBag = db.CardAccounts.Where(u => u.UserId == GetUserId());
+            ViewBag.Card = userCardForViewBag;
+
+            if (transferCard != null)
+            {
+                if (userCard.CardBalance >= cardAccountmodel.TransferBalance && cardAccountmodel.TransferBalance > 0)
+                {
+                    userCard.CardBalance = userCard.CardBalance - cardAccountmodel.TransferBalance;
+                    transferCard.CardBalance = transferCard.CardBalance + cardAccountmodel.TransferBalance;
+
+                    await db.SaveChangesAsync();
+
+                    return View();
+                }
+                ModelState.AddModelError("ErrorMessage_Id", "Insufficient funds, top up balance");
+                return View();
+            }
+            ModelState.AddModelError("ErrorMessage_Id", "Сard number not found");
+            return View();
+        }
+    }
+}
