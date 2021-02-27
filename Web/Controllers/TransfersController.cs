b73d62c50ea3c44265632e0038e0ac40bf725851
tilekuser <tilek.niyazbekov@mail.ru>
2021-02-27 20:39:53 +0600

    fix: добавить транзакцию и доработка отображения сообщений

--- Web/Controllers/TransfersController.cs
+++ Web/Controllers/TransfersController.cs
@@ -21,38 +21,52 @@ namespace Web.Controllers
         public async Task<IActionResult> Transfer()
         {
             var userCards = await db.CardAccounts.Where(u => u.UserId == UserId).ToArrayAsync();
-
-            return View(userCards);
+            var viewModel = new TransferViewModel
+            {
+                CardAccounts = userCards
+            };
+                   
+            return View(viewModel);
         }
 
         [HttpPost]
-        public async Task<IActionResult> Transfer(string selectCardNumber, string transferCardNumber, decimal transferBalance)
+        public async Task<IActionResult> Transfer(TransferViewModel transferViewModel)
         {
             var userCardView = db.CardAccounts.Where(u => u.UserId == UserId);
 
-            var userCard = await userCardView.FirstOrDefaultAsync(u => u.CardNumber == selectCardNumber);
-            var transferCard = await db.CardAccounts.FirstOrDefaultAsync(u => u.CardNumber == transferCardNumber);
+            var userCard = await userCardView.FirstOrDefaultAsync(u => u.CardNumber == transferViewModel.SelectCardNumber);
+            var transferCard = await db.CardAccounts.FirstOrDefaultAsync(u => u.CardNumber == transferViewModel.TransferCardNumber);
+
+            transferViewModel.CardAccounts = userCardView;
+
+
 
             if (userCard == null || transferCard == null)
             {
                 ModelState.AddModelError("ErrorMessage_Id", "Сard number not found");
 
-                return View(userCardView);
+                return View(transferViewModel);
             }
 
-            if (userCard.CardBalance < transferBalance || transferBalance <= 0)
+            if (userCard.CardBalance < transferViewModel.TransferBalance || transferViewModel.TransferBalance <= 0)
             {
                 ModelState.AddModelError("ErrorMessage_Id", "Insufficient funds, top up balance");
 
-                return View(userCardView);
+                return View(transferViewModel);
+            }
+            using(var transaction = await db.Database.BeginTransactionAsync())
+            {
+                userCard.CardBalance = userCard.CardBalance - transferViewModel.TransferBalance;
+                transferCard.CardBalance = transferCard.CardBalance + transferViewModel.TransferBalance;
+
+                await db.SaveChangesAsync();
+                await transaction.CommitAsync();
             }
 
-            userCard.CardBalance = userCard.CardBalance - transferBalance;
-            transferCard.CardBalance = transferCard.CardBalance + transferBalance;
+            TempData["SuccessMessage"] = $"Перевод успешно отправлен на сумму {transferViewModel.TransferBalance} KGS.";
 
-            await db.SaveChangesAsync();
 
-            return View(userCardView);
+            return RedirectToAction("Transfer");
         }
     }
 }
