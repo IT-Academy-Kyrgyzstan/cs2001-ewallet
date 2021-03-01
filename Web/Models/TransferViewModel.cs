69fa0841a2b2beb38581389bf159f2c6446d0b04
Tilek Kasymov <tilek.kasymov.01@gmail.com>
2021-03-01 13:54:03 +0600

    fix: добавить транзакцию, доработка отображения сообщений и устранение конфликтов

---
+++ Web/Models/TransferViewModel.cs
@@ -0,0 +1,28 @@
+﻿using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Models
+{
+    public class TransferViewModel
+    {
+        private IEnumerable<CardAccount> cardAccounts;
+        public IEnumerable<CardAccount> CardAccounts
+        {
+            get
+            {
+                return cardAccounts;
+            }
+            set
+            {
+                cardAccounts = value;
+            }
+        }
+
+        public string SelectCardNumber { get; set; }
+        public string TransferCardNumber { get; set; }
+        public decimal TransferBalance { get; set; }
+    }
+}
