955c0fe75debdb9ad76ee0546183ba21ef4160f4
Tilek Kasymov <tilek.kasymov.01@gmail.com>
2021-03-05 14:24:24 +0600

    add AspNetRoles

---
+++ Web/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataAccess;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Web.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminController : BaseController
+    {
+        public AdminController(ILogger<HomeController> logger, EwalletContext db) : base(logger, db)
+        { }
+
+        public IActionResult ApplicationsView()
+        {
+            var applications = db.CardApplications.ToList();
+            
+            return View(applications);
+        }
+    }
+}
