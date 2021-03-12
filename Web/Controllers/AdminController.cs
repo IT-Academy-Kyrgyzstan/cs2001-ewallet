3fe7b68b197f0a7083daff32949d4705e29f0922
Tilek Kasymov <tilek.kasymov.01@gmail.com>
2021-03-12 17:27:32 +0600

    feat/all-applications-for-issuing-card-for-operator

---
+++ Web/Controllers/AdminController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataAccess;
+using DataAccess.Enum;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class AdminController : BaseController
+    {
+        public AdminController(ILogger<HomeController> logger, EwalletContext db) : base(logger, db)
+        { }
+
+        [Authorize(Roles = "Operator")]
+        public IActionResult ApplicationsOperatorVM()
+        {
+            var viewOperator = from app in db.CardOrders
+                               select
+                               (new CardApplicationsOperatorVM
+                               {
+                                   Id = app.Id,
+                                   UserId = app.UserId,
+                                   Сurrency = app.Сurrency == Currency.KGS ? "KGS" : "Not specified",
+                                   CardType = app.CardType == CardType.Elcard ? "Elcard" : app.CardType == CardType.MasterCard ? "Master Card" : "Visa",
+                                   Status = app.Status == CardStatus.Approved ? "Approved" : app.Status == CardStatus.Considering ? "Considering" : "Denied",
+                                   CreatedDate = app.CreatedDate,
+                               }
+                               );
+
+            return View(viewOperator);
+        }
+    }
+}
---
+++ Web/Models/CardApplicationsOperatorVM.cs
@@ -0,0 +1,23 @@
+﻿using DataAccess.Enum;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Models
+{
+    public class CardApplicationsOperatorVM
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Сurrency { get; set; }
+        public string CardType { get; set; }
+        public string Status { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime DecisionDate { get; set; }
+
+    }
+}
